2581622e4cd45e82f009702e155c7a1ed8dc7e9b
Shohan <mjashohan@gmail.com>
2021-01-06 20:42:20 +0100

    Database initiated

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,26 @@ const cors = require('cors')
 const mysql = require('mysql')
 
 const app = express()
-app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+// setup database
+db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234567',
+    database: 'haat-protidin'
+  })
+
+  db.connect((err) => {
+        if(err){
+            throw err
+        }
+        console.log('Alhamdulillah, connection established')
+    })
+
+var server = {
+    port: 3000
+  }
+
+  app.listen( server.port , () => console.log(`Server started, listening port: ${server.port}`))
