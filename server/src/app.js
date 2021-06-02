d90c5f5709a1be4cf83fb60eb354345a15c8829e
Shohan <mjashohan@gmail.com>
2021-06-02 18:28:46 +0200

    Client side connected with the server side

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,18 @@ const cors = require('cors')
 const morgan = require('morgan')
 const config = require('./config/config')
 const db = require('./models/index')
+const path = require('path')
 
 const app = express()
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../../client/dist')))
 app.use(cors())
 app.use('/query', db)
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html/'));
+});
 
 app.listen( config.port , () => console.log(`Server started, listening port: ${config.port}`))
